0a520888634772d22212e7d9cc8dd9ec98603841
wajidashraf <wajidashraf673@gmail.com>
2026-05-15 15:46:26 +0500

    complete development

--- src/components/FusionPods/partner.tsx
+++ src/components/FusionPods/partner.tsx
@@ -8,7 +8,7 @@ const PartnerSection: React.FC = () => {
       <section className="max-w-[1280] mx-auto  py-12 px-6 md:px-8 lg:px-6 flex flex-col lg:flex-row items-start md:items-start lg:items-center gap-8 md:gap-10 lg:gap-12 2xl:gap-0">
         {/* Text Content */}
 
-        <p className="text-lg md:text-xl lg:text-xl xl:text-xl 2xl:text-xl text-[#5D5D5D] text-left mb-4 md:mb-0 max-w-full md:max-w-[65%] lg:max-w-[60%] xl:max-w-[55%] 2xl:max-w-[30%] md:ml-0 lg:ml-[3%] 2xl:ml-[5.5%]">
+        <p className="text-lg md:text-xl lg:text-xl xl:text-xl 2xl:text-xl text-[#5D5D5D] text-left mb-4 md:mb-0 max-w-full md:max-w-[65%] lg:max-w-[60%] xl:max-w-[55%] 2xl:max-w-[50%] md:ml-0 lg:ml-[3%] 2xl:ml-[3%]">
           Fusion Pods are used by Tech Startups, Microsoft MSPs, CSPs, ISV
           Partners, product companies, and enterprise IT teams to add delivery
           capacity quickly, without the overhead of permanent hiring or the risk
@@ -21,7 +21,7 @@ const PartnerSection: React.FC = () => {
         </p>
 
         {/* Logos */}
-        <div className="w-full md:w-auto lg:pl-0 xl:pl-8 2xl:pl-100 flex flex-col items-start md:items-center lg:items-start space-y-6">
+        <div className="w-full md:w-auto lg:pl-0 xl:pl-8 2xl:pl-30 flex flex-col items-start md:items-center lg:items-start space-y-6">
           <img
             src={partnerLogo}
             alt="Microsoft Partner"
